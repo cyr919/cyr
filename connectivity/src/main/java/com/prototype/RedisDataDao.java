5a9e49e41aa91bf3b74d8a3b53b2a4460d0afa41
cyr <cyr@192.168.56.1>
2020-04-23 13:18:53 +0900

    redis 기능 추가

--- connectivity/src/main/java/com/prototype/RedisDataDao.java
+++ connectivity/src/main/java/com/prototype/RedisDataDao.java
@@ -3,6 +3,10 @@
  */
 package com.prototype ;
 
+import java.util.ArrayList ;
+import java.util.List ;
+import java.util.Map ;
+
 import org.apache.log4j.Logger ;
 
 import redis.clients.jedis.Jedis ;
@@ -34,26 +38,22 @@ public class RedisDataDao
 		try {
 			String resulString = exe.redisHget( "PCS01" , "BP" ) ;
 			
-			
-			logger.info( resulString );
+			logger.info( resulString ) ;
 			
 			Long resultLong = exe.redisHset( "PCS01" , "BP" , "100.23" ) ;
-			 resulString = exe.redisHget( "PCS01" , "BP" ) ;
-							
-			logger.info( resultLong );
-			logger.info( resulString );
+			resulString = exe.redisHget( "PCS01" , "BP" ) ;
+			
+			logger.info( resultLong ) ;
+			logger.info( resulString ) ;
 			
 		}
 		catch( Exception e ) {
-			logger.error( e.getMessage( ) , e );
+			logger.error( e.getMessage( ) , e ) ;
 		}
-		
-		
 		
 	}
 	
 	/**
-	 * 
 	 * <pre>
 	 * Set the specified hash field to the specified value. 
 	 * If key does not exist, a new key holding a hash is created.
@@ -97,7 +97,6 @@ public class RedisDataDao
 	}
 	
 	/**
-	 * 
 	 * <pre>
 	 * If key holds a hash, retrieve the value associated to the specified field. 
 	 * If the field is not found or the key does not exist, a special 'nil' value is returned.
@@ -134,6 +133,74 @@ public class RedisDataDao
 		}
 		
 		return resultStr ;
+		
+	}
+	
+	/**
+	 * 
+	 * <pre>
+	 * Set the respective fields to the respective values. HMSET replaces old values with new values.
+	 * If key does not exist, a new key holding a hash is created.
+	 * </pre>
+	 * 
+	 * @author cyr
+	 * @date 2020-04-09
+	 * @param key
+	 * @param hash
+	 * @return Return OK or Exception if hash is empty
+	 * @throws Exception
+	 */
+	public String redisHmset( String key , Map< String , String > hash ) throws Exception {
+		
+		String resultStr = "" ;
+		Jedis jedis = null ;
+		
+		try {
+			jedis = JedisConnection.getJedisPool( ).getResource( ) ;
+			
+			logger.trace( "jedis.isConnected() :: " + jedis.isConnected( ) ) ;
+			if( jedis.isConnected( ) ) {
+				resultStr = jedis.hmset( key , hash ) ;
+			}
+		}
+		finally {
+			key = null ;
+			hash = null ;
+			
+			if( jedis != null ) {
+				jedis.close( ) ;
+			}
+			jedis = null ;
+		}
+		
+		return resultStr ;
+		
+	}
+	
+	public List< String > redisHmget( String key , String ... fields ) throws Exception {
+		
+		List< String > resultList = new ArrayList< String >( ) ;
+		Jedis jedis = null ;
+		
+		try {
+			jedis = JedisConnection.getJedisPool( ).getResource( ) ;
+			
+			logger.trace( "jedis.isConnected() :: " + jedis.isConnected( ) ) ;
+			if( jedis.isConnected( ) ) {
+				resultList = jedis.hmget( key , fields ) ;
+			}
+		}
+		finally {
+			key = null ;
+			fields = null ;
+			
+			if( jedis != null ) {
+				jedis.close( ) ;
+			}
+			jedis = null ;
+		}
+		
+		return resultList ;
 		
 	}
 	
