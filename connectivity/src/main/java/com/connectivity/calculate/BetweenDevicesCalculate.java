268dbabdf067cde755a96151529fece069dd9c53
cyr <cyr@DESKTOP-58HACMN>
2020-06-10 19:51:42 +0900

    장치간연산파일생성

---
+++ connectivity/src/main/java/com/connectivity/calculate/BetweenDevicesCalculate.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.connectivity.calculate ;
+
+import org.apache.logging.log4j.LogManager ;
+import org.apache.logging.log4j.Logger ;
+
+import com.connectivity.quality.QualityCode ;
+
+/**
+ * <pre>
+ * </pre>
+ *
+ * @author cyr
+ * @date 2020-06-10
+ */
+public class BetweenDevicesCalculate extends QualityCode implements Runnable
+{
+	
+	private Logger logger = LogManager.getLogger( this.getClass( ) ) ;
+	
+	/**
+	 * <pre>
+	 * </pre>
+	 * 
+	 * @author cyr
+	 * @date 2020-06-10
+	 * @param args
+	 */
+	public static void main( String[ ] args ) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void run( ) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
