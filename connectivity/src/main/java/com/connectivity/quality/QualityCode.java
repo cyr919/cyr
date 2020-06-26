5e12820d280aa4cd6bc9bcd8c2e15ec77e3390a5
cyr <cyr@DESKTOP-58HACMN>
2020-06-26 14:51:39 +0900

    QC 수정

--- connectivity/src/main/java/com/connectivity/quality/QualityCode.java
+++ connectivity/src/main/java/com/connectivity/quality/QualityCode.java
@@ -40,16 +40,30 @@ public class QualityCode
 	private CommUtil commUtil = new CommUtil( ) ;
 	
 	public QualityCode( ) {
-		this.recordQcInf = ConnectivityProperties.RECORD_QC_INF ;
-		this.recordQcIdx = ConnectivityProperties.RECORD_QC_IDX ;
-		this.fieldQcInf = ConnectivityProperties.FIELD_QC_INF ;
-		this.fieldQcIdx = ConnectivityProperties.FIELD_QC_IDX ;
-		this.strSiteSmlt = ConnectivityProperties.SITE_SMLT ;
-		this.strSiteSmltUsr = ConnectivityProperties.SITE_SMLT_USR ;
-		logger.debug( "recordQcInf :: " + System.identityHashCode( recordQcInf ) ) ;
-		logger.debug( "fieldQcInf :: " + System.identityHashCode( fieldQcInf ) ) ;
-		logger.debug( "this.recordQcInf :: " + System.identityHashCode( this.recordQcInf ) ) ;
-		logger.debug( "this.fieldQcInf :: " + System.identityHashCode( this.fieldQcInf ) ) ;
+		
+		synchronized( ConnectivityProperties.RECORD_QC_INF ) {
+			this.recordQcInf = ConnectivityProperties.RECORD_QC_INF ;
+		}
+		synchronized( ConnectivityProperties.RECORD_QC_IDX ) {
+			this.recordQcIdx = ConnectivityProperties.RECORD_QC_IDX ;
+		}
+		synchronized( ConnectivityProperties.FIELD_QC_INF ) {
+			this.fieldQcInf = ConnectivityProperties.FIELD_QC_INF ;
+		}
+		synchronized( ConnectivityProperties.FIELD_QC_IDX ) {
+			this.fieldQcIdx = ConnectivityProperties.FIELD_QC_IDX ;
+		}
+		synchronized( ConnectivityProperties.SITE_SMLT ) {
+			this.strSiteSmlt = ConnectivityProperties.SITE_SMLT ;
+		}
+		synchronized( ConnectivityProperties.SITE_SMLT_USR ) {
+			this.strSiteSmltUsr = ConnectivityProperties.SITE_SMLT_USR ;
+		}
+		
+		// logger.debug( "recordQcInf :: " + System.identityHashCode( recordQcInf ) ) ;
+		// logger.debug( "fieldQcInf :: " + System.identityHashCode( fieldQcInf ) ) ;
+		// logger.debug( "this.recordQcInf :: " + System.identityHashCode( this.recordQcInf ) ) ;
+		// logger.debug( "this.fieldQcInf :: " + System.identityHashCode( this.fieldQcInf ) ) ;
 	}
 	
 	// public QualityCode( HashMap< String , HashMap< String , Object > > recordQcInf , HashMap< String , HashMap< String , Integer > > recordQcIdx , HashMap< String , HashMap< String , Object > > fieldQcInf , HashMap< String , HashMap< String , Integer > > fieldQcIdx ) {
@@ -195,8 +209,8 @@ public class QualityCode
 		
 		try {
 			
-			logger.debug( "fieldQcInf.get( \"oldData\" ) :: " + fieldQcInf.get( "oldData" ) ) ;
-			logger.debug( "strDviceSkip :: " + strDviceSkip ) ;
+			// logger.debug( "fieldQcInf.get( \"oldData\" ) :: " + fieldQcInf.get( "oldData" ) ) ;
+			// logger.debug( "strDviceSkip :: " + strDviceSkip ) ;
 			
 			if( checkQualityCodeSkip( ( fieldQcInf.get( "oldData" ).get( "SKIP" ) + "" ) , strDviceSkip ) ) {
 				// 필드 QC 스킵 처리
@@ -208,7 +222,7 @@ public class QualityCode
 				
 				// OldData 판단
 				checkInt = connectivityProperties.CompareGatherDtmBeforeAndAfter( strGatherDmt , strDviceId ) ;
-				logger.debug( "checkInt :: " + checkInt ) ;
+				// logger.debug( "checkInt :: " + checkInt ) ;
 				if( checkInt > 2 ) {
 					// checkInt 가 3일 때 처리
 					// false
@@ -282,14 +296,14 @@ public class QualityCode
 	 * @author cyr
 	 * @date 2020-06-24
 	 * @param strQc 해당 mgp 키의 필드 QC
-	 * @param gatherValStr 계측 데이터
+	 * @param orgGatherValStr 계측 데이터
 	 * @param stdvDtMdlInfo 해당 mgp 키의 디바이스 데이터 모델 정보
 	 * @param tempRecordQc 임시 레코드 QC
 	 * @param gatherDataEventMap 이벤트 정보 맵(여기에 데이터 추가해주면 gather에서도 같이 추가된다. ... 참조형?)
 	 * @param strDviceSkip 디바이스 qc 스킵 여부
 	 * @return 계측 데이터의 필드 QC 결과 및 임시 레코드 QC
 	 */
-	public HashMap< String , Object > gatherOverFlow( String strQc , String gatherValStr , Map< String , Object > stdvDtMdlInfo , String tempRecordQc , HashMap< String , Object > gatherDataEventMap , String strDviceSkip ) {
+	public HashMap< String , Object > gatherOverFlow( String strQc , String orgGatherValStr , Map< String , Object > stdvDtMdlInfo , String tempRecordQc , HashMap< String , Object > gatherDataEventMap , String strDviceSkip ) {
 		HashMap< String , Object > resultMap = new HashMap< String , Object >( ) ;
 		
 		String resultStr = "" ;
@@ -303,11 +317,11 @@ public class QualityCode
 		HashMap< String , Object > eventDataMap = new HashMap< String , Object >( ) ;
 		
 		try {
-			logger.debug( "strQc :: " + strQc ) ;
-			logger.debug( "gatherValStr :: " + gatherValStr ) ;
-			logger.debug( "stdvDtMdlInfo :: " + stdvDtMdlInfo ) ;
-			logger.debug( "fieldQcIdx.get( \"overFlow\" ) :: " + fieldQcIdx.get( "overFlow" ) ) ;
-			logger.debug( "fieldQcInf.get( \"overFlow\" ) :: " + fieldQcInf.get( "overFlow" ) ) ;
+			// logger.debug( "strQc :: " + strQc ) ;
+			// logger.debug( "orgGatherValStr :: " + orgGatherValStr ) ;
+			// logger.debug( "stdvDtMdlInfo :: " + stdvDtMdlInfo ) ;
+			// logger.debug( "fieldQcIdx.get( \"overFlow\" ) :: " + fieldQcIdx.get( "overFlow" ) ) ;
+			// logger.debug( "fieldQcInf.get( \"overFlow\" ) :: " + fieldQcInf.get( "overFlow" ) ) ;
 			
 			if( checkQualityCodeSkip( ( fieldQcInf.get( "overFlow" ).get( "SKIP" ) + "" ) , strDviceSkip ) ) {
 				// 필드 QC 스킵 처리
@@ -322,7 +336,7 @@ public class QualityCode
 				if( !commUtil.checkNull( stdvDtMdlInfo.get( "MIN" ) + "" ) && !commUtil.checkNull( stdvDtMdlInfo.get( "MAX" ) + "" ) ) {
 					minData = new BigDecimal( stdvDtMdlInfo.get( "MIN" ) + "" ) ;
 					maxData = new BigDecimal( stdvDtMdlInfo.get( "MAX" ) + "" ) ;
-					valData = new BigDecimal( gatherValStr ) ;
+					valData = new BigDecimal( orgGatherValStr ) ;
 					
 					if( ( valData.compareTo( minData ) == -1 ) || ( valData.compareTo( maxData ) == 1 ) ) {
 						// gatherValStr < MIN || gatherValStr > MAX
@@ -366,7 +380,7 @@ public class QualityCode
 		}
 		finally {
 			strQc = null ;
-			gatherValStr = null ;
+			orgGatherValStr = null ;
 			stdvDtMdlInfo = null ;
 			tempRecordQc = null ;
 			gatherDataEventMap = null ;
@@ -399,9 +413,9 @@ public class QualityCode
 		String resultFieldQcStr = "" ;
 		
 		try {
-			logger.debug( "strQc :: " + strQc ) ;
-			logger.debug( "fieldQcIdx.get( \"measurementMode\" ) :: " + fieldQcIdx.get( "measurementMode" ) ) ;
-			logger.debug( "fieldQcInf.get( \"measurementMode\" ) :: " + fieldQcInf.get( "measurementMode" ) ) ;
+			// logger.debug( "strQc :: " + strQc ) ;
+			// logger.debug( "fieldQcIdx.get( \"measurementMode\" ) :: " + fieldQcIdx.get( "measurementMode" ) ) ;
+			// logger.debug( "fieldQcInf.get( \"measurementMode\" ) :: " + fieldQcInf.get( "measurementMode" ) ) ;
 			
 			if( checkQualityCodeSkip( ( fieldQcInf.get( "measurementMode" ).get( "SKIP" ) + "" ) , strDviceSkip ) ) {
 				// 필드 QC 스킵 처리
@@ -446,9 +460,9 @@ public class QualityCode
 		String resultFieldQcStr = "" ;
 		
 		try {
-			logger.debug( "strQc :: " + strQc ) ;
-			logger.debug( "fieldQcIdx.get( \"calculationMode\" ) :: " + fieldQcIdx.get( "calculationMode" ) ) ;
-			logger.debug( "fieldQcInf.get( \"calculationMode\" ) :: " + fieldQcInf.get( "calculationMode" ) ) ;
+			// logger.debug( "strQc :: " + strQc ) ;
+			// logger.debug( "fieldQcIdx.get( \"calculationMode\" ) :: " + fieldQcIdx.get( "calculationMode" ) ) ;
+			// logger.debug( "fieldQcInf.get( \"calculationMode\" ) :: " + fieldQcInf.get( "calculationMode" ) ) ;
 			
 			if( checkQualityCodeSkip( ( fieldQcInf.get( "calculationMode" ).get( "SKIP" ) + "" ) , strDviceSkip ) ) {
 				// 필드 QC 스킵 처리
@@ -494,10 +508,10 @@ public class QualityCode
 		String resultFieldQcStr = "" ;
 		
 		try {
-			logger.debug( "strQc :: " + strQc ) ;
-			logger.debug( "fieldQcIdx.get( \"simulationMode\" ) :: " + fieldQcIdx.get( "simulationMode" ) ) ;
-			logger.debug( "fieldQcInf.get( \"simulationMode\" ) :: " + fieldQcInf.get( "simulationMode" ) ) ;
-			logger.debug( "this.strSiteSmlt :: " + this.strSiteSmlt ) ;
+			// logger.debug( "strQc :: " + strQc ) ;
+			// logger.debug( "fieldQcIdx.get( \"simulationMode\" ) :: " + fieldQcIdx.get( "simulationMode" ) ) ;
+			// logger.debug( "fieldQcInf.get( \"simulationMode\" ) :: " + fieldQcInf.get( "simulationMode" ) ) ;
+			// logger.debug( "this.strSiteSmlt :: " + this.strSiteSmlt ) ;
 			
 			if( checkQualityCodeSkip( ( fieldQcInf.get( "simulationMode" ).get( "SKIP" ) + "" ) , strDviceSkip ) ) {
 				// 필드 QC 스킵 처리
