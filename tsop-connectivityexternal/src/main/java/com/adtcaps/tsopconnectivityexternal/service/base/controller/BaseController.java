f0515d584996e650c8cae60fd9d436d5b1e891cd
cyr <cyr@DESKTOP-58HACMN>
2020-10-14 00:57:14 +0900

    간단 컨드롤러 추가

---
+++ tsop-connectivityexternal/src/main/java/com/adtcaps/tsopconnectivityexternal/service/base/controller/BaseController.java
@@ -0,0 +1,79 @@
+package com.adtcaps.tsopconnectivityexternal.service.base.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping ;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * <ul>
+ * <li>업무 그룹명 : tsop-connectivity-external-dev</li>
+ * <li>서브 업무명 : com.adtcaps.tsop.service.vps.controller</li>
+ * <li>설  명 : BaseController.java</li>
+ * <li>작성일 : 2020. 9. 29.</li>
+ * <li>작성자 : jeonyb4</li>
+ * </ul>
+ */
+@RestController
+public class BaseController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	
+    
+	@SuppressWarnings("rawtypes")
+	@GetMapping( "/" )
+	public String index( ) {
+		// 화면에 모니터링 로그 남기기
+		logger.info( "index" ) ;
+		try {
+		}
+		catch( Exception e ) {
+			// 화면에 모니터링 로그 남기기
+			logger.error( e.getMessage( ) ) ;
+			logger.error( "{}", e ) ;
+		}
+		return "helloworld!";
+		
+	}
+	
+	
+    /**
+     * 
+     * listBuilding
+     *
+     * @return ResponseEntity
+     */
+//    @SuppressWarnings("rawtypes")
+//    @GetMapping(value = CONTEXT_URL + "/buildings")
+//    public ResponseEntity listBuilding() {
+//        
+//    	ResponseEntity<ResultDto> resEntity = null;
+//		String returnString = "";
+//    	
+//    	try {
+//    		
+//    		List<SiteResultDto> siteResultDtoList = new ArrayList<SiteResultDto>();
+//    		
+//    		SiteResultDto siteResultDto = new SiteResultDto();
+//    		siteResultDto.setSiteId("0001");
+//    		siteResultDto.setSiteName("SKT T-타워");
+//    		siteResultDto.setSiteAddress("서울특별시 중구 을지로 65");
+//    		siteResultDtoList.add(siteResultDto);
+//    		
+//    		returnString = Const.Common.RESULT_CODE.SUCCESS;
+//			resEntity = ResponseEntity.ok(new ResultDto(returnString, "", siteResultDtoList));
+//    		
+//    	} catch(Exception ex) {
+//            logger.error("listBuilding.Exception: {}", ex);
+//    		returnString = Const.Common.RESULT_CODE.FAIL;
+//    		resEntity = ResponseEntity.ok(new ResultDto(returnString, ex));
+//        }
+//    	return resEntity;
+//    }
+    
+    
+
+
+}
