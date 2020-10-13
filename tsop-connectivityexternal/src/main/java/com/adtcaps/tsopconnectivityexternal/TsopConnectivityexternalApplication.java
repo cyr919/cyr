3b20704b2c8abf9fd59107b611da0a98ce5e1e77
cyr <cyr@DESKTOP-58HACMN>
2020-10-13 22:13:24 +0900

    spring boot 시작하기 처음 버전

---
+++ tsop-connectivityexternal/src/main/java/com/adtcaps/tsopconnectivityexternal/TsopConnectivityexternalApplication.java
@@ -0,0 +1,13 @@
+package com.adtcaps.tsopconnectivityexternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsopConnectivityexternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsopConnectivityexternalApplication.class, args);
+	}
+
+}
---
+++ tsop-connectivityexternal/src/test/java/com/adtcaps/tsopconnectivityexternal/TsopConnectivityexternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adtcaps.tsopconnectivityexternal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsopConnectivityexternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
